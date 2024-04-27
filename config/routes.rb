fca62347657f92f0fb8eebf88e63c65dfa183f04
Noah Endo <noah.eyeing@gmail.com>
2024-04-28 00:24:09 +0900

    added calendar gem and migration file to adjust courses model

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root to: "pages#home"
+  root to: "courses#index"
   devise_for :users
 
   resources :courses do
@@ -10,6 +10,6 @@ Rails.application.routes.draw do
 
 
 
-  
+
   get "up" => "rails/health#show", as: :rails_health_check
 end
