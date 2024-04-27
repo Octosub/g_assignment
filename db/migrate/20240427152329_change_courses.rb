7be95d19d3dafede72221aef98e45e6fff922cd0
Noah Endo <noah.eyeing@gmail.com>
2024-04-28 00:26:16 +0900

    removed timeslot column from courses model and added start_time instead

--- db/migrate/20240427152329_change_courses.rb
+++ db/migrate/20240427152329_change_courses.rb
@@ -1,4 +1,6 @@
 class ChangeCourses < ActiveRecord::Migration[7.1]
   def change
+    remove_column :courses, :timeslot
+    add_column :courses, :start_time, :datetime
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_27_080809) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_27_152329) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -18,11 +18,11 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_27_080809) do
     t.string "title"
     t.text "description"
     t.string "semester"
-    t.integer "timeslot"
     t.integer "credits"
     t.bigint "teacher_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "start_time"
     t.index ["teacher_id"], name: "index_courses_on_teacher_id"
   end
 
