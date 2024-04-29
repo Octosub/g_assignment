17ad9cf5efd780164b945765aeed45dd2590737e
Noah Endo <noah.eyeing@gmail.com>
2024-04-29 15:53:08 +0900

    color coded timetable toggle button to indicate which timeatble is displayed

--- app/javascript/controllers/toggle_timetable_controller.js
+++ app/javascript/controllers/toggle_timetable_controller.js
@@ -7,7 +7,13 @@ export default class extends Controller {
     const wiseTimetable = event.currentTarget.parentElement.querySelector('#WiSe-timetable')
     const soseTimetable = event.currentTarget.parentElement.querySelector('#SoSe-timetable')
 
+    const wiseToggleButton = event.currentTarget.parentElement.querySelector('#WiSe-toggle-button')
+    const soseToggleButton = event.currentTarget.parentElement.querySelector('#SoSe-toggle-button')
+
     wiseTimetable.classList.toggle('d-none')
     soseTimetable.classList.toggle('d-none')
+
+    wiseToggleButton.classList.toggle('active-toggle')
+    soseToggleButton.classList.toggle('active-toggle')
   }
 }
