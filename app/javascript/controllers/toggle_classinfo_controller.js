1614e2e7d2e6f6b4b5d6ca4e9a624bca55bd587a
Noah Endo <noah.eyeing@gmail.com>
2024-04-29 19:22:21 +0900

    styled pop out info of course index

--- app/javascript/controllers/toggle_classinfo_controller.js
+++ app/javascript/controllers/toggle_classinfo_controller.js
@@ -4,7 +4,7 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
 
   toggle(event) {
-    const hiddenCourseCard = event.currentTarget.parentElement.querySelector('.hidden-course-card')
+    const hiddenCourseCard = event.currentTarget.parentElement.parentElement.parentElement.querySelector('.hidden-course-card')
     if (hiddenCourseCard) {
       hiddenCourseCard.classList.toggle('d-none')
     }
